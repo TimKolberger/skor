2b3d9a108692e1c638ccd7a9acdaad09f22453a3
Tim Kolberger <tim@kolberger.eu>
2022-10-03 23:03:53 +0200

    feat: add chakra ui theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,73 @@
+import { extendTheme } from "@chakra-ui/react"
+import "@fontsource/source-code-pro/300.css"
+import "@fontsource/source-code-pro/400.css"
+import "@fontsource/source-code-pro/900.css"
+
+import { colors } from "./colors"
+
+export const theme = extendTheme({
+  fonts: {
+    heading: `'Source Code Pro', sans-serif`,
+    body: `'Source Code Pro', sans-serif`,
+  },
+  config: {
+    initialColorMode: "dark",
+  },
+  colors,
+  semanticTokens: {
+    colors: {
+      "chakra-body-text": "gray.50",
+      "chakra-body-bg": "cyan.500",
+    },
+  },
+  styles: {
+    global: {
+      "*": {
+        WebkitTapHighlightColor: "rgba(0, 0, 0, 0)",
+      },
+      "html, body, #root": {
+        display: "flex",
+        flexDirection: "column",
+        flex: "1",
+        minHeight: "full",
+      },
+    },
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        textTransform: "uppercase",
+      },
+    },
+    Input: {
+      variants: {
+        filled: {
+          field: {
+            _placeholder: {
+              color: "whiteAlpha.700",
+            },
+            _focusVisible: {
+              outline: "0",
+              borderColor: "blue.200",
+            },
+          },
+        },
+      },
+    },
+    NumberInput: {
+      variants: {
+        filled: {
+          field: {
+            _placeholder: {
+              color: "whiteAlpha.700",
+            },
+            _focusVisible: {
+              outline: "0",
+              borderColor: "blue.200",
+            },
+          },
+        },
+      },
+    },
+  },
+})
