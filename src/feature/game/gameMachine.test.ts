122a5e4811c5ea0ca1a8b87ff4adc03081b7ec8a
Tim Kolberger <tim@kolberger.eu>
2022-10-09 01:10:11 +0200

    test: add game machine tests as todo

---
+++ src/feature/game/gameMachine.test.ts
@@ -0,0 +1,12 @@
+describe("GameMachine", () => {
+  it.todo("should increment")
+  it.todo("should decrement")
+  it.todo("should set score")
+  it.todo("should go idle")
+  it.todo("should set players")
+  it.todo("should set step")
+  it.todo("should set sort")
+  it.todo("should toggle sort")
+})
+
+export {}
