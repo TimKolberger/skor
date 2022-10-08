90cc4811f7d589abe5880fd7d94462c4a01cd029
Tim Kolberger <tim@kolberger.eu>
2022-10-09 01:10:04 +0200

    test: add player machine tests as todo

---
+++ src/feature/players/playerMachine.test.ts
@@ -0,0 +1,8 @@
+describe("PlayerMachine", () => {
+  it.todo("should add player")
+  it.todo("should update player")
+  it.todo("should remove player")
+  it.todo("should clear players")
+})
+
+export {}
