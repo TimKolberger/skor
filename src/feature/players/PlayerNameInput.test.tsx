911067b6778e533c225b07edc536e09d96c9a11d
Tim Kolberger <tim@kolberger.eu>
2022-10-09 12:46:01 +0200

    test: add PlayerNameInput tests

---
+++ src/feature/players/PlayerNameInput.test.tsx
@@ -0,0 +1,38 @@
+import { fireEvent, screen } from "@testing-library/react"
+import { render } from "test/utils"
+
+import { PlayerNameInput } from "./PlayerNameInput"
+
+describe("PlayerNameInput", () => {
+  it("should show the initial value", () => {
+    const onChangeMock = jest.fn()
+    render(<PlayerNameInput value="player name" onChange={onChangeMock} />)
+
+    const input = screen.getByRole("textbox", { name: "Player name" })
+
+    expect(input).toHaveValue("player name")
+  })
+
+  it("should trigger the onChange when updated", () => {
+    const onChangeMock = jest.fn()
+    render(<PlayerNameInput value="player name" onChange={onChangeMock} />)
+
+    const input = screen.getByRole("textbox", { name: "Player name" })
+    fireEvent.change(input, { target: { value: "updated name" } })
+
+    expect(onChangeMock).toHaveBeenCalledWith("updated name")
+  })
+
+  it("should generate a random player name", () => {
+    const onChangeMock = jest.fn()
+    render(<PlayerNameInput value="player name" onChange={onChangeMock} />)
+
+    const generateNameButton = screen.getByRole("button", {
+      name: "Generate player name",
+    })
+    fireEvent.click(generateNameButton)
+
+    expect(onChangeMock).not.toHaveBeenCalledWith("player name")
+    expect(onChangeMock).toHaveBeenCalledWith(expect.any(String))
+  })
+})
