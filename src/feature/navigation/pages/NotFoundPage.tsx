2ece1a35cbf0e0baf8e35b763b0e46202adaade3
Tim Kolberger <tim@kolberger.eu>
2022-10-09 19:53:20 +0200

    chore: move navigation pages

---
+++ src/feature/navigation/pages/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+import { Alert, AlertTitle } from "@chakra-ui/react"
+import * as React from "react"
+
+import { FullModalLayout } from "../../../layouts/FullModalLayout"
+import { Main } from "../../../layouts/Main"
+
+export default function NotFoundPage() {
+  return (
+    <FullModalLayout>
+      <Main>
+        <Alert>
+          <AlertTitle>Page not found</AlertTitle>
+        </Alert>
+      </Main>
+    </FullModalLayout>
+  )
+}
--- src/feature/navigation/routes.tsx
+++ src/feature/navigation/routes.tsx
@@ -2,15 +2,21 @@ import * as React from "react"
 
 import { linker } from "./linker"
 
-const LazyGameScorePage = React.lazy(() => import("../scores/GameScoresPage"))
-const LazyEditPlayerPage = React.lazy(() => import("../players/EditPlayerPage"))
-const LazyAddPlayerPage = React.lazy(() => import("../players/AddPlayerPage"))
-const LazyNotFoundPage = React.lazy(() => import("./NotFoundPage"))
+const LazyGameScorePage = React.lazy(
+  () => import("../scores/pages/GameScoresPage")
+)
+const LazyEditPlayerPage = React.lazy(
+  () => import("../players/pages/EditPlayerPage")
+)
+const LazyAddPlayerPage = React.lazy(
+  () => import("../players/pages/AddPlayerPage")
+)
+const LazyNotFoundPage = React.lazy(() => import("./pages/NotFoundPage"))
 const LazySetAllScoresPage = React.lazy(
-  () => import("../scores/SetAllScoresPage")
+  () => import("../scores/pages/SetAllScoresPage")
 )
 const LazyPlayerScorePage = React.lazy(
-  () => import("../scores/PlayerScorePage")
+  () => import("../scores/pages/PlayerScorePage")
 )
 const LazySettingsPage = React.lazy(() => import("../settings/SettingsPage"))
 const LazyLegalNoticePage = React.lazy(
