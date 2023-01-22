15fad4ac07eb11536184ed9eaa695c50c9719860
Tim Kolberger <tim@kolberger.eu>
2023-01-22 21:22:59 +0100

    feat: add clear button

--- src/feature/players/PlayerNameInput.tsx
+++ src/feature/players/PlayerNameInput.tsx
@@ -2,11 +2,14 @@ import {
   chakra,
   FormControl,
   FormLabel,
+  Icon,
   IconButton,
   Input,
+  InputGroup,
+  InputRightElement,
 } from "@chakra-ui/react"
 import * as React from "react"
-import { FiShuffle } from "react-icons/fi"
+import { FiShuffle, FiX } from "react-icons/fi"
 
 import { generatePlayerName } from "./generatePlayerName"
 
@@ -19,15 +22,24 @@ export const PlayerNameInput = ({ value, onChange }: PlayerNameInputProps) => (
   <FormControl>
     <FormLabel>Player name</FormLabel>
     <chakra.div display="flex" gap="3">
-      <Input
-        size="lg"
-        variant="filled"
-        placeholder="e.g. John Doe"
-        required
-        name="playerName"
-        value={value}
-        onChange={({ currentTarget: { value } }) => onChange(value)}
-      />
+      <InputGroup size="lg" variant="filled">
+        <Input
+          placeholder="e.g. John Doe"
+          required
+          name="playerName"
+          value={value}
+          onChange={({ currentTarget: { value } }) => onChange(value)}
+        />
+        <InputRightElement>
+          <IconButton
+            size="sm"
+            aria-label="Clear"
+            icon={<Icon as={FiX} fontSize="xl" />}
+            variant="ghost"
+            onClick={() => onChange("")}
+          />
+        </InputRightElement>
+      </InputGroup>
       <IconButton
         size="lg"
         aria-label="Generate player name"
