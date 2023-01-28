e30d2ca665d87bb0accd308ee66dfcfc3a735c88
Tim Kolberger <tim@kolberger.eu>
2023-01-28 18:15:10 +0100

    chore: refactor routes

--- src/feature/navigation/routes.tsx
+++ src/feature/navigation/routes.tsx
@@ -1,45 +1,47 @@
-import * as React from "react"
+import { createElement, lazy } from "react"
 
-import { RouteObject } from "react-router-dom"
 import { linker } from "./linker"
 
-function lazyRoute(fn: () => Promise<{ default: React.ComponentType }>) {
-  return async () => ({
-    Component: (await fn()).default,
-  })
-}
-
-export const routes: RouteObject[] = [
+export const routes = [
   {
     path: linker.home.definition,
-    lazy: lazyRoute(() => import("../scores/pages/GameScoresPage")),
+    element: createElement(
+      lazy(() => import("../scores/pages/GameScoresPage"))
+    ),
   },
   {
     path: linker.playerScore.definition,
-    lazy: lazyRoute(() => import("../scores/pages/PlayerScorePage")),
+    element: createElement(
+      lazy(() => import("../scores/pages/PlayerScorePage"))
+    ),
   },
   {
     path: linker.addPlayer.definition,
-    lazy: lazyRoute(() => import("../players/pages/AddPlayerPage")),
+    element: createElement(
+      lazy(() => import("../players/pages/AddPlayerPage"))
+    ),
   },
   {
     path: linker.editPlayer.definition,
-    lazy: lazyRoute(() => import("../players/pages/EditPlayerPage")),
+    element: createElement(
+      lazy(() => import("../players/pages/EditPlayerPage"))
+    ),
   },
   {
     path: linker.setScores.definition,
-    lazy: lazyRoute(() => import("../scores/pages/SetAllScoresPage")),
+    element: createElement(
+      lazy(() => import("../scores/pages/SetAllScoresPage"))
+    ),
   },
   {
     path: linker.settings.definition,
-    lazy: lazyRoute(() => import("../settings/SettingsPage")),
+    element: createElement(lazy(() => import("../settings/SettingsPage"))),
   },
   {
     path: linker.legalNotice.definition,
-    lazy: lazyRoute(() => import("../legal-notice/LegalNoticePage")),
+    element: createElement(
+      lazy(() => import("../legal-notice/LegalNoticePage"))
+    ),
   },
-  {
-    path: "*",
-    lazy: lazyRoute(() => import("./pages/NotFoundPage")),
-  },
+  { element: createElement(lazy(() => import("./pages/NotFoundPage"))) },
 ]
