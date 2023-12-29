ed2065c60bd86e5dded7b38527869ef1f5085dd5
Tim Kolberger <tim@kolberger.eu>
2023-12-29 13:57:37 +0100

    chore: replace ariakit with radix due bundle size

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,68 +1,64 @@
 import { buttonClasses, type ButtonVariants } from './button.tsx'
-import {
-  Menu as AriaMenu,
-  MenuButton as AriaMenuButton,
-  type MenuButtonProps,
-  MenuItem as AriaMenuItem,
-  type MenuItemProps,
-  type MenuProps,
-  MenuProvider as AriaMenuProvider,
-  type MenuProviderProps,
-  MenuSeparator as AriaMenuSeparator,
-  type MenuSeparatorProps,
-} from '@ariakit/react'
-import clsx from 'clsx'
+import * as RadixMenu from '@radix-ui/react-dropdown-menu'
+import { clsx } from 'clsx'
 
-export const Menu = (props: MenuProviderProps) => {
+export const Menu = (props: RadixMenu.DropdownMenuProps) => {
   const { children } = props
-  return <AriaMenuProvider>{children}</AriaMenuProvider>
+  return <RadixMenu.Root>{children}</RadixMenu.Root>
 }
 
-export const MenuContent = (props: MenuProps) => {
+export const MenuContent = (props: RadixMenu.DropdownMenuContentProps) => {
   const { className, ...rest } = props
   return (
-    <AriaMenu
-      gutter={8}
-      {...rest}
-      className={clsx(
-        className,
-        'rounded border border-slate-50 border-opacity-10 bg-cyan-700 shadow-lg',
-      )}
-    />
+    <RadixMenu.Portal>
+      <RadixMenu.Content
+        sideOffset={8}
+        {...rest}
+        className={clsx(
+          className,
+          'z-40 rounded border border-slate-50 border-opacity-10 bg-cyan-700 shadow-lg focus-visible:ring-0',
+        )}
+      />
+    </RadixMenu.Portal>
   )
 }
 
-export const MenuItem = (props: MenuItemProps) => {
+export const MenuItem = (props: RadixMenu.MenuItemProps) => {
   const { className, ...rest } = props
 
   return (
-    <AriaMenuItem
+    <RadixMenu.Item
       {...rest}
       className={clsx(
         className,
         'flex items-center gap-3 px-3 py-2',
         'bg-slate-50 bg-opacity-0',
-        'transition hover:bg-opacity-5 focus-visible:bg-opacity-5',
+        'transition hover:bg-opacity-5 focus-visible:bg-opacity-5 focus-visible:ring-0',
         'cursor-pointer',
       )}
     />
   )
 }
 
-export const MenuSeparator = (props: MenuSeparatorProps) => {
+export const MenuSeparator = (props: RadixMenu.DropdownMenuSeparatorProps) => {
   const { className, ...rest } = props
   return (
-    <AriaMenuSeparator
+    <RadixMenu.Separator
       {...rest}
-      className={clsx(className, 'my-1 border-slate-50 border-opacity-25')}
+      className={clsx(
+        className,
+        'my-1 border-t border-slate-50 border-opacity-25',
+      )}
     />
   )
 }
 
-export const MenuButton = (props: MenuButtonProps & ButtonVariants) => {
+export const MenuButton = (
+  props: RadixMenu.DropdownMenuTriggerProps & ButtonVariants,
+) => {
   const { className, variant, padding, size, ...rest } = props
   return (
-    <AriaMenuButton
+    <RadixMenu.Trigger
       {...rest}
       className={clsx(className, buttonClasses({ variant, padding, size }))}
     />
