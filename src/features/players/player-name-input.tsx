82ff397c83d4fab7597379f91d40be1e895460da
Tim Kolberger <tim@kolberger.eu>
2023-12-30 14:32:35 +0100

    feat: autofocus input fields

--- src/features/players/player-name-input.tsx
+++ src/features/players/player-name-input.tsx
@@ -1,5 +1,6 @@
 import { IconButton } from '../../components/button.tsx'
 import { generatePlayerName } from './generate-player-name.ts'
+import { forwardRef } from 'react'
 import { FiShuffle, FiX } from 'react-icons/fi'
 
 interface PlayerNameInputProps {
@@ -7,35 +8,44 @@ interface PlayerNameInputProps {
   onChange: (playerName: string) => void
 }
 
-export const PlayerNameInput = ({ value, onChange }: PlayerNameInputProps) => (
-  <label className="flex flex-col gap-1">
-    Player name
-    <div className="flex items-center gap-3">
-      <div className="flex flex-1 items-center rounded bg-slate-200 bg-opacity-10 py-1 pl-4 pr-1">
-        <input
-          placeholder="e.g. John Doe"
-          required
-          name="playerName"
-          value={value}
-          onChange={({ currentTarget: { value } }) => onChange(value)}
-          className="bg-transparent placeholder:text-slate-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-transparent"
-        />
+export const PlayerNameInput = forwardRef<
+  HTMLInputElement,
+  PlayerNameInputProps
+>((props, ref) => {
+  const { value, onChange } = props
+  return (
+    <label className="flex flex-col gap-1">
+      Player name
+      <div className="flex items-center gap-3">
+        <div className="flex flex-1 items-center rounded bg-slate-200 bg-opacity-10 py-1 pl-4 pr-1">
+          <input
+            placeholder="e.g. John Doe"
+            required
+            name="name"
+            value={value}
+            onChange={({ currentTarget: { value } }) => onChange(value)}
+            className="flex-1 bg-transparent placeholder:text-slate-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-transparent"
+            ref={ref}
+            autoComplete="off"
+            inputMode="text"
+          />
+          <IconButton
+            size="sm"
+            aria-label="Clear"
+            variant="ghost"
+            onClick={() => onChange('')}
+            className="ml-auto"
+          >
+            <FiX className="text-xl" />
+          </IconButton>
+        </div>
         <IconButton
-          size="sm"
-          aria-label="Clear"
-          variant="ghost"
-          onClick={() => onChange('')}
-          className="ml-auto"
+          aria-label="Generate player name"
+          onClick={() => onChange(generatePlayerName())}
         >
-          <FiX className="text-xl" />
+          <FiShuffle />
         </IconButton>
       </div>
-      <IconButton
-        aria-label="Generate player name"
-        onClick={() => onChange(generatePlayerName())}
-      >
-        <FiShuffle />
-      </IconButton>
-    </div>
-  </label>
-)
+    </label>
+  )
+})
